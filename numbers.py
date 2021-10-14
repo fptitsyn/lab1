727a61d0490c49efa8a9c7ca35fe835f2c9b2664
fptitsyn <fedya.ptitsyn@mail.ru>
2021-10-14 10:16:30 +0300

    Added numbers.py

---
+++ numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Input first number: "))
+b = int(input("Input second number: "))
+c = int(input("Input third number: "))
+d = int(input("Input fourth number: "))
+
+sum1 = a + b
+sum2 = c + d
+
+print("{:.2f}".format(sum1 / sum2))
