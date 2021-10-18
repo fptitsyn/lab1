4b7f09438937cfd6dc6422e31be3378b96d73586
fptitsyn <fedya.ptitsyn@mail.ru>
2021-10-18 20:44:43 +0300

    Renamed user.py

--- individual.py
+++ individual.py
@@ -1,6 +1,14 @@
-first = []
-first = input("Input cords separated with a whitespace").split()
-second = []
-second = input("Input cords separated with a whitespace").split()
-third = []
-third = input("Input cords separated with a whitespace").split()
+import math
+
+first = input("Input cords separated with a whitespace: ")
+second = input("Input cords separated with a whitespace: ")
+third = input("Input cords separated with a whitespace: ")
+
+a1 = first.split()
+a2 = second.split()
+a3 = second.split()
+
+for i in range(2):
+    a1[i] = int(a1[i])
+
+d1 = math.sqrt((second[1] - second[0]) ** 2 + (first[1] - first[0]) ** 2)
