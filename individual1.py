f0cae039da626f42dc2e822795432392b3a7ea46
fptitsyn <fedya.ptitsyn@mail.ru>
2021-10-18 20:58:52 +0300

    Finished individual task

--- individual1.py
+++ individual1.py
@@ -1,14 +1,20 @@
 import math
 
-first = input("Input cords separated with a whitespace: ")
-second = input("Input cords separated with a whitespace: ")
-third = input("Input cords separated with a whitespace: ")
-
-a1 = first.split()
-a2 = second.split()
-a3 = second.split()
+first = input("Input cords separated with a whitespace: ").split()
+second = input("Input cords separated with a whitespace: ").split()
+third = input("Input cords separated with a whitespace: ").split()
 
 for i in range(2):
-    a1[i] = int(a1[i])
+    first[i] = int(first[i])
+    second[i] = int(second[i])
+    third[i] = int(third[i])
 
 d1 = math.sqrt((second[1] - second[0]) ** 2 + (first[1] - first[0]) ** 2)
+d2 = math.sqrt((third[1] - third[0]) ** 2 + (second[1] - second[0]) ** 2)
+d3 = math.sqrt((first[1] - first[0]) ** 2 + (third[1] - third[0]) ** 2)
+
+P = d1 + d2 + d3
+p = P / 2
+S = math.sqrt(p * (p - d1) * (p - d2) * (p - d3))
+
+print("P =", P, "S =", S)
